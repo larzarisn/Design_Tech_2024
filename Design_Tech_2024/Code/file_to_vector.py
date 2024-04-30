005c35f32c46322829865e313b45c87794fddcdd
larzarisn <75928465+larzarisn@users.noreply.github.com>
2024-04-30 11:25:53 +1000

    Added project because this aparently did not work?

---
+++ Design_Tech_2024/Code/file_to_vector.py
@@ -0,0 +1,57 @@
+import os
+import index
+import cairo
+import re
+
+def vector_creator(gerber_file_path, gerber_file_name, settings):
+    gerber_file_extensionless = gerber_file_name.partition(".")[0]
+    print(f"./Vectors/{gerber_file_extensionless}.svg")
+    with cairo.SVGSurface(f"./Vectors/{gerber_file_extensionless}.svg", 700, 700) as svg_layer: 
+        cairo_context = cairo.Context(svg_layer)
+        gerber_file = open(gerber_file_path, "r")   
+        instruction_array = gerber_file.readlines()
+        # have to loop over twice: once to get the vectors of the shapes and what tool to use, then to actually make the shapes in the svg
+        # pg.26-> on gerber layer format pdf to specify which each thing is
+        # have to make a list of tools with different sizes and etc....
+        # then use that list to loop over the rest of the commands
+        # possibly using {shape:[vectors]} as the structure (which will make this ugly)
+        # then a for loop over each shape, and adding to svg
+        # might want to make a line break process to watch it happen to an svg to understand it.
+        aperture_instruction_dict = {}
+        instruction_string = "".join(instruction_array)
+        aperture_list = re.findall("ADD([0-9][0-9])(.*)?\*%", instruction_string) 
+        scale = int(re.findall("FSLAX[1-6]([1-9])Y", instruction_string)[0])
+        print(scale)
+        current_aperture = "NaN"
+        for aperture in aperture_list:
+            aperture_instruction_dict[aperture[0]] = aperture[1].split(",") # makes an aperture with the shape information and the size information
+        for instruction in instruction_array:
+            if re.match(".*D([1-9][0-9]).*", instruction):
+                print("stroke!")
+                current_aperture = re.findall(".*D([1-9][0-9]).*", instruction)[0] # This gives the number of the current aperture: that can be fed into the known dict to get the width and the shape
+                #cairo_context.scale(700, 700)
+                cairo_context.stroke()
+
+            if instruction.startswith("X"):
+                cairo_context.set_line_width(float(aperture_instruction_dict[current_aperture][1])) 
+                cairo_context.set_line_cap(1)
+                cairo_context.set_source_rgba(0, 0, 1, 1)
+                to_x_coord = float(re.findall("X(.*)Y", instruction)[0])*pow(10, -1*scale)
+                to_y_coord = float(re.findall("Y(.*)D", instruction)[0])*pow(10, -1*scale)
+                print(f"current coordinates: y: {to_y_coord} x: {to_x_coord}  using aperture {current_aperture} with width of {cairo_context.get_line_width()}")
+
+                if re.match(".*D01.*", instruction):
+                    cairo_context.line_to(to_x_coord, to_y_coord)
+                    #cairo_context.stroke()
+
+                if re.match(".*D02.*", instruction):
+                    cairo_context.move_to(to_x_coord, to_y_coord)
+                    #cairo_context.stroke()
+
+                if re.match(".*D03.*", instruction): # This needs to move to a point, create a single aperture point and stop.
+                    cairo_context.move_to(to_x_coord, to_y_coord)
+                    cairo_context.line_to(to_x_coord, to_y_coord)
+                    #cairo_context.stroke()
+        # cairo_context.scale(700, 700)
+        cairo_context.stroke()
+    return "balls"
---
+++ Design_Tech_2024/Code/index.py
@@ -0,0 +1,38 @@
+# this file will be the main loop of the program, it will have the GUI and the functions linkback.
+import os
+import file_to_vector
+
+folder_path = "C:/Users/larsn/Desktop/Design Tech 2024 git/Design_Tech_2024/Fritzing/Temp testing" # replace this with a dynamic link, will happen with GUI
+
+gerber_files = os.listdir(folder_path)
+
+gerber_layers = {}
+ 
+for gerber_file in gerber_files: # loop over all gerber files and sort them into layers, this will get messy, needs fixing once operational.
+    gerber_file_path = folder_path + "/" + gerber_file
+    print(gerber_file_path)
+
+    if "bottom" in gerber_file.lower():
+        try:
+            print(gerber_file)
+            testing = file_to_vector.vector_creator(gerber_file_path, gerber_file, 0)
+            gerber_layers["bottom"].append(gerber_file)
+        except Exception as exception:
+            print(exception)
+            gerber_layers["bottom"] = [gerber_file]
+    elif "top" in gerber_file.lower():
+        try:
+            # testing = file_to_vector.vector_creator(gerber_file_path, gerber_file, 0)
+            gerber_layers["top"].append(gerber_file)
+        except Exception as exception:
+            print(exception)
+            gerber_layers["top"] = [gerber_file]
+    else:
+        try:
+            gerber_layers["other"].append(gerber_file)
+        except:
+            print("else")
+            gerber_layers["other"] = [gerber_file]
+
+# now gerber_layers is a dict with split layers that can be used to turn the layers into independant images.
+print(gerber_layers)
