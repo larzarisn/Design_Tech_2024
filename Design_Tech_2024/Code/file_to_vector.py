f009dfd457ad75e17cda8116a6dcb1440d8cb37b
larzarisn <lars.nolles@gmail.com>
2024-05-18 21:23:48 +1000

    Made box outlines around lines

--- Design_Tech_2024/Code/file_to_vector.py
+++ Design_Tech_2024/Code/file_to_vector.py
@@ -1,11 +1,11 @@
 import os
-import index
 import cairo
 import re
 import math
 
-# draw_line function turns one line into two to make it parsable by parametric editing software.
-def draw_line(x_coord, y_coord, start_x_coord, start_y_coord, current_width, cairo_context): 
+# return_line_coordinates takes the current trace and returns the coordinates of its edges.
+# This is useful to make the traces continuous because otherwise there would be gaps in traces for each time a new coordinate is given.
+def return_line_coordinates(x_coord, y_coord, start_x_coord, start_y_coord, current_width): 
     # making function to calculate the outline of the main traces to work with freeCAD.
     # to make this I will make an x/y offset and move the aperture by that much
     # this offset requires a calculation of the hypotonuse/length of the line as well as the distance that the aperture moves in both x and y.
@@ -18,60 +18,95 @@ def draw_line(x_coord, y_coord, start_x_coord, start_y_coord, current_width, cai
     y_offset = (0.5*delta_x*current_width)/segment_length
     # this works by making a triangle out of the main line segment and equating it to the sin/cos of the offset triangle.
     # now these offsets can be used to draw two lines instead of one.
-    for i in [-1, 1]: # this means that i can be either -1, or 1 allowing to create two lines with either a positive or a negative offset.
 
-        x_starting_position = start_x_coord + i*x_offset 
-        y_starting_position = start_y_coord + i*y_offset
-        
-        x_ending_position = x_coord + i*x_offset
-        y_ending_position = y_coord + i*y_offset
+    positive_x_starting_position = start_x_coord - x_offset 
+    positive_y_starting_position = start_y_coord + y_offset
+    
+    positive_x_ending_position = x_coord - x_offset
+    positive_y_ending_position = y_coord + y_offset
 
-        cairo_context.move_to(x_starting_position, y_starting_position) # move to starting point
-        cairo_context.line_to(x_ending_position, y_ending_position) # and draw
-        print(f"going from current coordinates: y: {y_starting_position} x: {x_starting_position} to y: {y_ending_position} x: {x_ending_position}  with width of {cairo_context.get_line_width()}")
-        print(delta_x)
+    negative_x_starting_position = start_x_coord + x_offset 
+    negative_y_starting_position = start_y_coord - y_offset
+    
+    negative_x_ending_position = x_coord + x_offset
+    negative_y_ending_position = y_coord - y_offset
+
+    # returning points in touple form to make it easier to identify x,y.
+    return [[positive_x_starting_position, positive_y_starting_position], [positive_x_ending_position, positive_y_ending_position], [negative_x_starting_position, negative_y_starting_position], [negative_x_ending_position, negative_y_ending_position]] 
+
+# draws the trace from start to finish, capping off the end.
+def draw_trace(point_dict, cairo_context):
+
+    if len(point_dict["positive"]) == 0: # Checking if there's actually anything in the list
+        return
+
+    cairo_context.set_line_width(1) 
+    cairo_context.set_source_rgba(0, 1, 1, 1)
+    # Turning the edge coordinates into a list of commands to run through.
+    point_list = []
+    print(point_dict["negative"])
+    print(list(reversed(point_dict["negative"])))
+    for point in point_dict["positive"]:
+        point_list.append(point)
+    for point in list(reversed(point_dict["negative"])):
+        point_list.append(point)    
+    # Running through the commands
+    cairo_context.move_to(point_list[0][0], point_list[0][1])
+    for point in point_list: 
+        cairo_context.line_to(point[0], point[1])
+    cairo_context.line_to(point_list[0][0], point_list[0][1])
+    cairo_context.stroke()
 
 def vector_creator(gerber_file_path, gerber_file_name, settings):
     gerber_file_extensionless = gerber_file_name.partition(".")[0]
-    print(f"./Vectors/{gerber_file_extensionless}.svg")
-    with cairo.SVGSurface(f"./Vectors/{gerber_file_extensionless}.svg", 700, 700) as svg_layer: 
+    # print(f"./Design_Tech_2024/Vectors/{gerber_file_extensionless}.svg")
+    with cairo.SVGSurface(f"./Design_Tech_2024/Vectors/{gerber_file_extensionless}.svg", 1500, 1500) as svg_layer: 
         # user variables to be set
-        vector_scale_factor = 10
+        vector_scale_factor = 500
         # getting variables ready
         cairo_context = cairo.Context(svg_layer)
         gerber_file = open(gerber_file_path, "r")   
         instruction_array = gerber_file.readlines()
         aperture_instruction_dict = {}
         instruction_string = "".join(instruction_array)
-        aperture_list = re.findall("ADD([0-9][0-9])(.*)?\*%", instruction_string) 
+        aperture_list = re.findall("ADD([0-9][0-9])(.*)?/*%", instruction_string) 
         scale = int(re.findall("FSLAX[1-6]([1-9])Y", instruction_string)[0])
         current_aperture = "NaN" 
         last_x_coord = 0 # setting this for later because of weird for loop behavior.
         last_y_coord = 0
+        trace_edge_dict = {"positive":[], "negative":[]} # Stores the line start and end locations for each part of the line post offset.
 
         for aperture in aperture_list:
             aperture_instruction_dict[aperture[0]] = aperture[1].split(",") # makes an aperture with the shape information and the size information
         for instruction in instruction_array:
             if re.match(".*D([1-9][0-9]).*", instruction):
-                print("stroke!")
+                #print("stroke!")
                 current_aperture = re.findall(".*D([1-9][0-9]).*", instruction)[0] # This gives the number of the current aperture: that can be fed into the known dict to get the width and the shape
                 cairo_context.stroke() # outputting each line/shape here onto the canvas
  
             if instruction.startswith("X"):
-                cairo_context.set_line_width(float(aperture_instruction_dict[current_aperture][1])*vector_scale_factor) 
-                cairo_context.set_line_cap(1)
+                cairo_context.set_line_width(float(aperture_instruction_dict[current_aperture][1].replace("*", ""))*vector_scale_factor) 
                 cairo_context.set_source_rgba(0, 0, 1, 1)
+                cairo_context.set_line_cap(1)
                 to_x_coord = float(re.findall("X(.*)Y", instruction)[0])*pow(10, -1*scale)*vector_scale_factor
                 to_y_coord = float(re.findall("Y(.*)D", instruction)[0])*pow(10, -1*scale)*vector_scale_factor
-                print(f"current coordinates: y: {to_y_coord} x: {to_x_coord}  using aperture {current_aperture} with width of {cairo_context.get_line_width()}")
+                #print(f"current coordinates: y: {to_y_coord} x: {to_x_coord}  using aperture {current_aperture} with width of {cairo_context.get_line_width()}")
 
                 if re.match(".*D01.*", instruction):
                     # cairo_context.line_to(to_x_coord, to_y_coord)
-                    draw_line(to_x_coord, to_y_coord, last_x_coord, last_y_coord, cairo_context.get_line_width(), cairo_context)
+                    line_edge_array = return_line_coordinates(to_x_coord, to_y_coord, last_x_coord, last_y_coord, cairo_context.get_line_width())
+                    for positive_edge_coordinates in line_edge_array[0: 2]:
+                        trace_edge_dict["positive"].append(positive_edge_coordinates)
+                    for negative_edge_coordinates in line_edge_array[2: 4]:
+                        trace_edge_dict["negative"].append(negative_edge_coordinates)
                 if re.match(".*D02.*", instruction):
+                    draw_trace(trace_edge_dict, cairo_context)
+                    trace_edge_dict = {"positive":[], "negative":[]} # resets the edge coordinates for new trace
                     cairo_context.move_to(to_x_coord, to_y_coord)
 
                 if re.match(".*D03.*", instruction): # This needs to move to a point, create a single aperture point and stop. (ie it makes a circle with the apertures width)
+                    draw_trace(trace_edge_dict, cairo_context)
+                    trace_edge_dict = {"positive":[], "negative":[]} # resets the edge coordinates for new trace
                     cairo_context.move_to(to_x_coord, to_y_coord) 
                     cairo_context.line_to(to_x_coord, to_y_coord)
                     # LOOK INTO pycairos PATTERNS FOR THIS
@@ -79,5 +114,5 @@ def vector_creator(gerber_file_path, gerber_file_name, settings):
                 last_x_coord = to_x_coord 
                 last_y_coord = to_y_coord 
         cairo_context.stroke()
-    return "balls"
+    return "testing123"
 
--- Design_Tech_2024/Code/index.py
+++ Design_Tech_2024/Code/index.py
@@ -1,39 +1,40 @@
 # this file will be the main loop of the program, it will have the GUI and the functions linkback.
 import os
-import file_to_vector
+from file_to_vector import *
 
 folder_path = "C:/Users/lars/Desktop/Design_Tech_2024/Design_Tech_2024/Fritzing/Temp testing" # replace this with a dynamic link, will happen with GUI
 
 gerber_files = os.listdir(folder_path)
 
 gerber_layers = {}
- 
-with cairo.SVGSurface(f"./Vectors/{gerber_file_extensionless}.svg", 700, 700) as svg_layer: 
-    
 
-for gerber_file in gerber_files: # loop over all gerber files and sort them into layers, this will get messy, needs fixing once operational.
-    gerber_file_path = folder_path + "/" + gerber_file
+temp_file_path = "C:/Users/lars/Desktop/Design_Tech_2024/Design_Tech_2024/Fritzing/Temp testing/ProcessingRGB_copperBottom.gbl"
+testing = vector_creator(temp_file_path, "ProcessingRGB_copperBottom.gbl", 0)     
 
-    if "bottom" in gerber_file.lower():
-        try:
-            testing = file_to_vector.vector_creator(gerber_file_path, gerber_file, 0)
-            gerber_layers["bottom"].append(gerber_file)
-        except Exception as exception:
-            print(exception)
-            gerber_layers["bottom"] = [gerber_file]
-    elif "top" in gerber_file.lower():
-        try:
-            # testing = file_to_vector.vector_creator(gerber_file_path, gerber_file, 0)
-            gerber_layers["top"].append(gerber_file)
-        except Exception as exception:
-            print(exception)
-            gerber_layers["top"] = [gerber_file]
-    else:
-        try:
-            gerber_layers["other"].append(gerber_file)
-        except:
-            print("else")
-            gerber_layers["other"] = [gerber_file]
+# for gerber_file in gerber_files: # loop over all gerber files and sort them into layers, this will get messy, needs fixing once operational.
+#     gerber_file_path = folder_path + "/" + gerber_file
+
+#     if "bottom" in gerber_file.lower():
+#         try:
+#             testing = vector_creator(gerber_file_path, gerber_file, 0)
+#             gerber_layers["bottom"].append(gerber_file)
+#         except Exception as exception:
+#             print(exception)
+#             gerber_layers["bottom"] = [gerber_file]
+#     elif "top" in gerber_file.lower():
+#         try:
+#             # testing = file_to_vector.vector_creator(gerber_file_path, gerber_file, 0)
+#             gerber_layers["top"].append(gerber_file)
+#         except Exception as exception:
+#             print(exception)
+#             gerber_layers["top"] = [gerber_file]
+#     else:
+#         try:
+#             gerber_layers["other"].append(gerber_file)
+#         except:
+#             print("else")
+#             gerber_layers["other"] = [gerber_file]
 
 # now gerber_layers is a dict with split layers that can be used to turn the layers into independant images.
+
 print(gerber_layers)
