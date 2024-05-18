90b86e3906c2da726aa7b5f3c09ccbb3f6d5a8db
larzarisn <lars.nolles@gmail.com>
2024-05-18 23:26:23 +1000

    Connected boxes working, however it would require a complete rework to work properly

--- Design_Tech_2024/Code/file_to_vector.py
+++ Design_Tech_2024/Code/file_to_vector.py
@@ -19,17 +19,17 @@ def return_line_coordinates(x_coord, y_coord, start_x_coord, start_y_coord, curr
     # this works by making a triangle out of the main line segment and equating it to the sin/cos of the offset triangle.
     # now these offsets can be used to draw two lines instead of one.
 
-    positive_x_starting_position = start_x_coord - x_offset 
-    positive_y_starting_position = start_y_coord + y_offset
+    positive_x_starting_position = start_x_coord + x_offset 
+    positive_y_starting_position = start_y_coord - y_offset
     
-    positive_x_ending_position = x_coord - x_offset
-    positive_y_ending_position = y_coord + y_offset
+    positive_x_ending_position = x_coord + x_offset
+    positive_y_ending_position = y_coord - y_offset
 
-    negative_x_starting_position = start_x_coord + x_offset 
-    negative_y_starting_position = start_y_coord - y_offset
+    negative_x_starting_position = start_x_coord - x_offset 
+    negative_y_starting_position = start_y_coord + y_offset
     
-    negative_x_ending_position = x_coord + x_offset
-    negative_y_ending_position = y_coord - y_offset
+    negative_x_ending_position = x_coord - x_offset
+    negative_y_ending_position = y_coord + y_offset
 
     # returning points in touple form to make it easier to identify x,y.
     return [[positive_x_starting_position, positive_y_starting_position], [positive_x_ending_position, positive_y_ending_position], [negative_x_starting_position, negative_y_starting_position], [negative_x_ending_position, negative_y_ending_position]] 
@@ -72,9 +72,8 @@ def vector_creator(gerber_file_path, gerber_file_name, settings):
         aperture_list = re.findall("ADD([0-9][0-9])(.*)?/*%", instruction_string) 
         scale = int(re.findall("FSLAX[1-6]([1-9])Y", instruction_string)[0])
         current_aperture = "NaN" 
-        last_x_coord = 0 # setting this for later because of weird for loop behavior.
-        last_y_coord = 0
         trace_edge_dict = {"positive":[], "negative":[]} # Stores the line start and end locations for each part of the line post offset.
+        last_points = []
 
         for aperture in aperture_list:
             aperture_instruction_dict[aperture[0]] = aperture[1].split(",") # makes an aperture with the shape information and the size information
@@ -85,34 +84,59 @@ def vector_creator(gerber_file_path, gerber_file_name, settings):
                 cairo_context.stroke() # outputting each line/shape here onto the canvas
  
             if instruction.startswith("X"):
+
+                # setting up the svg
                 cairo_context.set_line_width(float(aperture_instruction_dict[current_aperture][1].replace("*", ""))*vector_scale_factor) 
                 cairo_context.set_source_rgba(0, 0, 1, 1)
                 cairo_context.set_line_cap(1)
                 to_x_coord = float(re.findall("X(.*)Y", instruction)[0])*pow(10, -1*scale)*vector_scale_factor
                 to_y_coord = float(re.findall("Y(.*)D", instruction)[0])*pow(10, -1*scale)*vector_scale_factor
-                #print(f"current coordinates: y: {to_y_coord} x: {to_x_coord}  using aperture {current_aperture} with width of {cairo_context.get_line_width()}")
 
-                if re.match(".*D01.*", instruction):
-                    # cairo_context.line_to(to_x_coord, to_y_coord)
-                    line_edge_array = return_line_coordinates(to_x_coord, to_y_coord, last_x_coord, last_y_coord, cairo_context.get_line_width())
-                    for positive_edge_coordinates in line_edge_array[0: 2]:
-                        trace_edge_dict["positive"].append(positive_edge_coordinates)
-                    for negative_edge_coordinates in line_edge_array[2: 4]:
-                        trace_edge_dict["negative"].append(negative_edge_coordinates)
+                # Last points is used to try and match lines together.
+                if len(last_points) <= 3:
+                    last_points.append([to_x_coord, to_y_coord])
+                else: 
+                    last_points.pop(0)
+                    last_points.append([to_x_coord, to_y_coord])
+
+                if re.match(".*D01.*", instruction): # This is the draw instruction
+                    # Instead of actually drawing in this step, the coordinates of the edges of the line are saved so that the line can be made in one batch later.
+                    
+                    # This checks if the line that is being drawn meets the last (or one of the last lines) to be drawn
+                    lines_meet_boolean = False
+                    for point in last_points[0:3]:
+                        if to_x_coord == point[0] and to_y_coord == point[1]:
+                            lines_meet_boolean = True
+                    # If it is false it traces out the line
+                    if not lines_meet_boolean:
+                        draw_trace(trace_edge_dict, cairo_context)
+                        trace_edge_dict = {"positive":[], "negative":[]} # resets the edge coordinates for new trace
+                        line_edge_array = return_line_coordinates(last_points[-2][0], last_points[-2][1], to_x_coord, to_y_coord, float(aperture_instruction_dict[current_aperture][1].replace("*", ""))*vector_scale_factor)
+                        # line_edge_array = return_line_coordinates(to_x_coord, to_y_coord, last_points[-2][0], last_points[-2][1], float(aperture_instruction_dict[current_aperture][1].replace("*", ""))*vector_scale_factor)
+                        for positive_edge_coordinates in line_edge_array[0: 2]:
+                            trace_edge_dict["positive"].append(positive_edge_coordinates)
+                        for negative_edge_coordinates in line_edge_array[2: 4]:
+                            trace_edge_dict["negative"].append(negative_edge_coordinates)
+                    else:
+                        line_edge_array = return_line_coordinates(last_points[-2][0], last_points[-2][1], to_x_coord, to_y_coord, float(aperture_instruction_dict[current_aperture][1].replace("*", ""))*vector_scale_factor)
+                        for positive_edge_coordinates in line_edge_array[0: 2]:
+                            trace_edge_dict["positive"].append(positive_edge_coordinates)
+                        for negative_edge_coordinates in line_edge_array[2: 4]:
+                            trace_edge_dict["negative"].append(negative_edge_coordinates)
+                    #cairo_context.set_source_rgba(0, 1, 0, 1)
+                    #cairo_context.line_to(to_x_coord, to_y_coord)
                 if re.match(".*D02.*", instruction):
-                    draw_trace(trace_edge_dict, cairo_context)
-                    trace_edge_dict = {"positive":[], "negative":[]} # resets the edge coordinates for new trace
+                    # draw_trace(trace_edge_dict, cairo_context)
+                    # trace_edge_dict = {"positive":[], "negative":[]} # resets the edge coordinates for new trace
                     cairo_context.move_to(to_x_coord, to_y_coord)
 
                 if re.match(".*D03.*", instruction): # This needs to move to a point, create a single aperture point and stop. (ie it makes a circle with the apertures width)
-                    draw_trace(trace_edge_dict, cairo_context)
-                    trace_edge_dict = {"positive":[], "negative":[]} # resets the edge coordinates for new trace
+                    # draw_trace(trace_edge_dict, cairo_context)
+                    # trace_edge_dict = {"positive":[], "negative":[]} # resets the edge coordinates for new trace
                     cairo_context.move_to(to_x_coord, to_y_coord) 
                     cairo_context.line_to(to_x_coord, to_y_coord)
                     # LOOK INTO pycairos PATTERNS FOR THIS
-                # This tells the drawline function where the last known coord was and is useful to make the D03 work properly later.
-                last_x_coord = to_x_coord 
-                last_y_coord = to_y_coord 
+        draw_trace(trace_edge_dict, cairo_context)
         cairo_context.stroke()
     return "testing123"
 
