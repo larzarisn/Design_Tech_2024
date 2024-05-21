96327887cf8174d29fa0fec5a8cda43daeb6b993
larzarisn <lars.nolles@gmail.com>
2024-05-21 19:42:34 +1000

    Added circular end caps to lines

--- Design_Tech_2024/Code/file_to_vector.py
+++ Design_Tech_2024/Code/file_to_vector.py
@@ -15,40 +15,34 @@ def return_circle_coordinates(delta_x, delta_y, width, segment_length, x, y, t):
 
 # return_line_coordinates takes the current trace and returns the coordinates of its edges.
 # This is useful to make the traces continuous because otherwise there would be gaps in traces for each time a new coordinate is given.
-def return_line_coordinates(x_coord, y_coord, start_x_coord, start_y_coord, current_width): 
+def return_line_coordinates(x_coord, y_coord, start_x_coord, start_y_coord, current_width, num_circle_points): 
     # making function to calculate the outline of the main traces to work with freeCAD.
     # to make this I will make an x/y offset and move the aperture by that much
     # this offset requires a calculation of the hypotonuse/length of the line as well as the distance that the aperture moves in both x and y.
+    return_point_list = []
+
     delta_x = x_coord - start_x_coord
     delta_y = y_coord - start_y_coord
     # using pythag for the length:
     segment_length = math.hypot(delta_x, delta_y)
 
-    positive_starting_position = return_circle_coordinates(delta_x, -delta_y, current_width, segment_length, start_x_coord, start_y_coord, math.pi)
+    for i in range(num_circle_points + 2):
+        t = i*(math.pi/(num_circle_points + 1))
+        point_gen = return_circle_coordinates(-delta_x, delta_y, current_width, segment_length, x_coord, y_coord, t)
+        return_point_list.append(point_gen)
 
-    negative_starting_position = return_circle_coordinates(delta_x, -delta_y, current_width, segment_length, start_x_coord, start_y_coord, 0)
-
-    positive_ending_position = return_circle_coordinates(-delta_x, delta_y, current_width, segment_length, x_coord, y_coord, 0)
-    
-    negative_ending_position = return_circle_coordinates(-delta_x, delta_y, current_width, segment_length, x_coord, y_coord, math.pi)
+    for i in range(num_circle_points + 2):
+        t = i*(math.pi/(num_circle_points + 1))
+        point_gen = return_circle_coordinates(delta_x, -delta_y, current_width, segment_length, start_x_coord, start_y_coord, t)
+        return_point_list.append(point_gen)
 
     # returning points in touple form to make it easier to identify x,y.
-    return [positive_starting_position, positive_ending_position, negative_starting_position, negative_ending_position] 
+    return return_point_list
 
 # draws the trace from start to finish, capping off the end.
-def draw_trace(point_dict, cairo_context):
-
-    if len(point_dict["positive"]) == 0: # Checking if there's actually anything in the list
-        return
-
+def draw_trace(point_list, cairo_context):
     cairo_context.set_line_width(1) 
     cairo_context.set_source_rgba(0, 1, 1, 1)
-    # Turning the edge coordinates into a list of commands to run through.
-    point_list = []
-    for point in point_dict["positive"]:
-        point_list.append(point)
-    for point in list(reversed(point_dict["negative"])):
-        point_list.append(point)    
     # Running through the commands
     cairo_context.move_to(point_list[0][0], point_list[0][1])
     for point in point_list: 
@@ -62,6 +56,7 @@ def vector_creator(gerber_file_path, gerber_file_name, settings):
     with cairo.SVGSurface(f"./Design_Tech_2024/Vectors/{gerber_file_extensionless}.svg", 1500, 1500) as svg_layer: 
         # user variables to be set
         vector_scale_factor = 500
+        num_circle_points = 0
         # getting variables ready
         cairo_context = cairo.Context(svg_layer)
         gerber_file = open(gerber_file_path, "r")   
@@ -92,28 +87,20 @@ def vector_creator(gerber_file_path, gerber_file_name, settings):
                 #print(f"current coordinates: y: {to_y_coord} x: {to_x_coord}  using aperture {current_aperture} with width of {cairo_context.get_line_width()}")
 
                 if re.match(".*D01.*", instruction):
-                    line_edge_array = return_line_coordinates(to_x_coord, to_y_coord, last_x_coord, last_y_coord, cairo_context.get_line_width())
-                    for positive_edge_coordinates in line_edge_array[0: 2]:
-                        trace_edge_dict["positive"].append(positive_edge_coordinates)
-                    for negative_edge_coordinates in line_edge_array[2: 4]:
-                        trace_edge_dict["negative"].append(negative_edge_coordinates)
+                    line_edge_array = return_line_coordinates(to_x_coord, to_y_coord, last_x_coord, last_y_coord, cairo_context.get_line_width(), num_circle_points)
+                    draw_trace(line_edge_array, cairo_context)
                     cairo_context.set_source_rgba(0, 1, 0, 1)
-                    # cairo_context.line_to(to_x_coord, to_y_coord)
-                    
+
                 if re.match(".*D02.*", instruction):
-                    draw_trace(trace_edge_dict, cairo_context)
-                    trace_edge_dict = {"positive":[], "negative":[]} # resets the edge coordinates for new trace
                     cairo_context.move_to(to_x_coord, to_y_coord)
 
                 if re.match(".*D03.*", instruction): # This needs to move to a point, create a single aperture point and stop. (ie it makes a circle with the apertures width)
-                    draw_trace(trace_edge_dict, cairo_context)
-                    trace_edge_dict = {"positive":[], "negative":[]} # resets the edge coordinates for new trace
                     cairo_context.move_to(to_x_coord, to_y_coord) 
                     cairo_context.line_to(to_x_coord, to_y_coord)
-                    # LOOK INTO pycairos PATTERNS FOR THIS
+
                 # This tells the drawline function where the last known coord was and is useful to make the D03 work properly later.
                 last_x_coord = to_x_coord 
                 last_y_coord = to_y_coord 
-        draw_trace(trace_edge_dict, cairo_context)
+        # draw_trace(trace_edge_dict, cairo_context)
         cairo_context.stroke()
     return "testing123"
